65deb59ceb685f2b108a096a25983027eaf3d340
Vardaan Kapoor <vardaan123454321@gmail.com>
2024-11-15 23:48:14 -0800

    Cleaning up runParser

--- runParser.sh
+++ runParser.sh
@@ -1,28 +1,27 @@
 #!/bin/bash
+
+# Clean previous build artifacts and generate DAT files
 make clean
 make createDATfiles
-#sort sell.dat | uniq -D
-# this command shows the duplicating groups-this will show all 11 items one after the other(sorted by each of the 4 groups)
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' sell.dat
-#this command shows number of repeating/duplicating GROUPS(if 4 groups have 11 items repeating as 3,4,2,2 duplicates then answer wil be 4)
-awk -i inplace '!seen[$0]++' sell.dat
-#this command deletes the duplicate tuples/lins from the files
-#sort items.dat | uniq -D
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' items.dat
-awk -i inplace '!seen[$0]++' items.dat
-#sort user.dat | uniq -D
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' user.dat
-awk -i inplace '!seen[$0]++' user.dat
-#sort seller.dat | uniq -D
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' seller.dat
-awk -i inplace '!seen[$0]++' seller.dat
-#sort bids.dat | uniq -D
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' bids.dat
-awk -i inplace '!seen[$0]++' bids.dat
-#sort bidder.dat | uniq -D
-#awk '{ if (seen[$0]++) { count++; } } END { print count }' bidder.dat
-awk -i inplace '!seen[$0]++' bidder.dat
+
+# Deduplicate entries in various data files
+# The following commands remove duplicate lines from each file in-place.
+# Note: Duplicate tuples/lines are identified and removed using `awk`.
+
+files=("sell.dat" "items.dat" "user.dat" "seller.dat" "bids.dat" "bidder.dat")
+
+for file in "${files[@]}"; do
+    if [[ -f $file ]]; then
+        echo "Processing $file..."
+        awk -i inplace '!seen[$0]++' "$file"
+    else
+        echo "Warning: $file not found. Skipping..."
+    fi
+done
+
+# Create the database, load data, and run the queries
 make makeAdatabase
 make loadData
 make run7queries
+
 
